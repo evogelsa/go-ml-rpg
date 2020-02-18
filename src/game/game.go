87508aa544be29b9f383a62b314b94cdeafc326b
Ethan Vogelsang <evogelsa@iu.edu>
2020-02-17 19:38:35 -0500

    add game class framework and tester

---
+++ src/game/game.go
@@ -0,0 +1,346 @@
+// TODO
+// make attacks event based (no absolute damage)
+// will require making function for turn / round processing
+// add the other Classes and attacks
+// make cli based ui for testing
+// port to web
+
+package game
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type Move int
+
+const (
+	HEAVY Move = iota
+	QUICK
+	STANDARD
+	BLOCK
+	PARRY
+	EVADE
+)
+
+// Class is the base player struct storing stats and data
+type Class struct {
+	PlayerName string
+	ClassName  string
+	Health     int     // capped 100
+	Stamina    int     // capped 100
+	Armor      int     // capped 20
+	Strength   float32 // normalized
+	Dexterity  float32 // normalized
+	Intellect  float32 // normalized
+}
+
+// NewKnight generates a new knight class with initial values
+func NewKnight(playerName string) Class {
+	return Class{
+		PlayerName: playerName,
+		ClassName:  "Knight",
+		Health:     rand.Intn(20+1) + 80,
+		Stamina:    rand.Intn(20+1) + 50,
+		Armor:      rand.Intn(20 + 1),
+		Strength:   float32(rand.Intn(5+1)+15) / 20,
+		Dexterity:  float32(rand.Intn(5+1)+10) / 20,
+		Intellect:  float32(rand.Intn(5+1)+5) / 20,
+	}
+}
+
+// NewArcher generates a new arhcer lcass with initial values
+func NewArcher(playerName string) Class {
+	return Class{
+		PlayerName: playerName,
+		ClassName:  "Archer",
+		Health:     rand.Intn(20+1) + 80,
+		Stamina:    rand.Intn(20+1) + 50,
+		Armor:      rand.Intn(20 + 1),
+		Strength:   float32(rand.Intn(5+1)+5) / 20,
+		Dexterity:  float32(rand.Intn(5+1)+15) / 20,
+		Intellect:  float32(rand.Intn(5+1)+10) / 20,
+	}
+}
+
+// NewWizard generates a new wizard class with initial values
+func NewWizard(playerName string) Class {
+	return Class{
+		PlayerName: playerName,
+		ClassName:  "Wizard",
+		Health:     rand.Intn(20+1) + 80,
+		Stamina:    rand.Intn(20+1) + 50,
+		Armor:      rand.Intn(20 + 1),
+		Strength:   float32(rand.Intn(5+1)+10) / 20,
+		Dexterity:  float32(rand.Intn(5+1)+5) / 20,
+		Intellect:  float32(rand.Intn(5+1)+15) / 20,
+	}
+}
+
+// heavyAttack deals damaged based off of attackers strength but
+// success probability is determined based off defenders intellect
+func (c *Class) heavyAttack(e *Class) int {
+	// higher enemy intellect -> lower chance to hit
+	if rand.Float32() > e.Intellect {
+		// higher attacker strength -> more damage
+		return int(float32(rand.Intn(20+1))*c.Strength + 1.5)
+	} else {
+		return 0
+	}
+}
+
+// quickAttack deals damaged based off of attackers dexterity but
+// success probability is determined based off defenders strength
+func (c *Class) quickAttack(e *Class) int {
+	// higher enemy strength -> lower chance to hit
+	if rand.Float32() > e.Strength {
+		// higher attacker dexterity -> more damage
+		return int(float32(rand.Intn(20+1))*c.Dexterity + 1.5)
+	} else {
+		return 0
+	}
+}
+
+// standardAttack deals damaged based off of attacker intellect but
+// success probability is determined based off defenders dexterity
+func (c *Class) standardAttack(e *Class) int {
+	// higer enemy dexterity -> lower chance to hit
+	if rand.Float32() > e.Dexterity {
+		// higher attacker intellect -> more damage
+		return int(float32(rand.Intn(20+1))*c.Intellect + 1.5)
+	} else {
+		return 0
+	}
+}
+
+// block attempts to block an attack using strength to determine
+// success. on success blocker takes no damage and repairs armor
+func (c *Class) blockDefense(e *Class) int {
+	if rand.Float32() < c.Strength {
+		return rand.Intn(int(c.Strength*10 + .5))
+	} else {
+		return 0
+	}
+}
+
+// parry attempts to dodge an attack and counter, success based on
+// dexterity of person parrying, failure results in damaging self
+func (c *Class) parryDefense(e *Class) int {
+	if rand.Float32() < c.Dexterity {
+		return rand.Intn(int(c.Dexterity*10 + .5))
+	} else {
+		return -rand.Intn(int(c.Dexterity*10 + .5))
+	}
+}
+
+// evade attempts to evade an attack using intelligence to
+// determine success. on success evader takes no damage and heals
+func (c *Class) evadeDefense(e *Class) int {
+	if rand.Float32() < c.Intellect {
+		return rand.Intn(int(c.Intellect*10 + .5))
+	} else {
+		return 0
+	}
+}
+
+func PrintCharacter(p Class) {
+	fmt.Printf("********************\n* %-16s *\n********************\n",
+		p.PlayerName)
+	fmt.Printf("* Health: %-8d *\n", p.Health)
+	fmt.Printf("* Armor: %-9d *\n", p.Armor)
+	fmt.Printf("* Strength: %-6d *\n", int(p.Strength*100))
+	fmt.Printf("* Dexterity: %-5d *\n", int(p.Dexterity*100))
+	fmt.Printf("* Intellect: %-5d *\n", int(p.Intellect*100))
+	fmt.Printf("********************\n")
+}
+
+func parseMove(p1, p2 *Class, m Move) (bool, int) {
+	var def bool
+	var r int
+	switch m {
+	case HEAVY:
+		def = false
+		r = p1.heavyAttack(p2)
+	case QUICK:
+		def = false
+		r = p1.quickAttack(p2)
+	case STANDARD:
+		def = false
+		r = p1.standardAttack(p2)
+	case BLOCK:
+		def = true
+		r = p1.blockDefense(p2)
+	case PARRY:
+		def = true
+		r = p1.parryDefense(p2)
+	case EVADE:
+		def = true
+		r = p1.evadeDefense(p2)
+	}
+
+	return def, r
+}
+
+// handleDamage parses damage to determine armor and health effects
+func handleDamage(p *Class, d int) {
+	// armor absorbs all damage as long as armor health >0
+	if p.Armor > 0 {
+		p.Armor -= d
+		if p.Armor < 0 {
+			p.Armor = 0
+		}
+	} else {
+		p.Health -= d
+	}
+}
+
+// printTurn prints out text regarding outcome of turn
+func printTurn(p1, p2 *Class, m1, m2 Move, s1, s2 bool) {
+	var moveNames = map[Move]string{
+		HEAVY:    "heavy attack",
+		QUICK:    "quick attack",
+		STANDARD: "standard attack",
+		BLOCK:    "block",
+		PARRY:    "parry",
+		EVADE:    "evade",
+	}
+	var result = map[bool]string{
+		false: "fails",
+		true:  "succeeds",
+	}
+
+	fmt.Printf("%s attempts to %s %s and %s\n",
+		p1.PlayerName, moveNames[m1], p2.PlayerName, result[s1])
+	fmt.Printf("%s attempts to %s %s and %s\n",
+		p2.PlayerName, moveNames[m2], p1.PlayerName, result[s2])
+}
+
+func printStats(p *Class) {
+	fmt.Printf("%s\n", p.PlayerName)
+	fmt.Printf("\tHealth: %d\n", p.Health)
+	fmt.Printf("\tArmor: %d\n", p.Armor)
+}
+
+func Turn(p1, p2 *Class, m1, m2 Move) {
+	def1, a1 := parseMove(p1, p2, m1)
+	def2, a2 := parseMove(p2, p1, m2)
+	if !def1 && !def2 {
+		// both players attack, handle damage
+		var s1, s2 bool
+		if a1 > 0 {
+			handleDamage(p2, a1)
+			s1 = true
+		}
+		if a2 > 0 {
+			handleDamage(p1, a2)
+			s2 = true
+		}
+		printTurn(p1, p2, m1, m2, s1, s2)
+		if s1 {
+			fmt.Printf("%s deals %d damage\n", p1.PlayerName, a1)
+		}
+		if s2 {
+			fmt.Printf("%s deals %d damage\n", p2.PlayerName, a2)
+		}
+	} else if !def1 && def2 {
+		// if player2 defends then use attack of player1 to
+		// determine the outcome
+		switch m2 {
+		case BLOCK:
+			// if p2 success, repair armor of p2, else p2
+			// takes damage of p1
+			if a2 > 0 {
+				p2.Armor += a2
+				printTurn(p1, p2, m1, m2, false, true)
+				fmt.Printf("%s repairs armor for %d points\n", p2.PlayerName, a2)
+			} else if a1 > 0 {
+				handleDamage(p2, a1)
+				printTurn(p1, p2, m1, m2, true, false)
+				fmt.Printf("%s deals %d damage\n", p1.PlayerName, a1)
+			} else {
+				printTurn(p1, p2, m1, m2, false, false)
+			}
+		case PARRY:
+			// if p2 success, p1 takes damage of p1 attack +
+			// damage of p2 counter, vise versa if fail
+			if a2 > 0 {
+				handleDamage(p1, a1+a2)
+				printTurn(p1, p2, m1, m2, false, true)
+				fmt.Printf("%s deals %d damage\n", p2.PlayerName, a1+a2)
+			} else if a1-a2 > 0 {
+				handleDamage(p2, a1-a2)
+				printTurn(p1, p2, m1, m2, true, false)
+				fmt.Printf("%s deals %d damage\n", p1.PlayerName, a1-a2)
+			} else {
+				printTurn(p1, p2, m1, m2, false, false)
+			}
+		case EVADE:
+			// if p2 success p2 heals a little, on fail p2
+			// takes damage of p1 attack
+			if a2 > 0 {
+				p2.Health += a2
+				printTurn(p1, p2, m1, m2, false, true)
+				fmt.Printf("%s heals %d damage\n", p2.PlayerName, a2)
+			} else if a1 > 0 {
+				handleDamage(p2, a1)
+				printTurn(p1, p2, m1, m2, true, false)
+				fmt.Printf("%s deals %d damage\n", p1.PlayerName, a1)
+			} else {
+				printTurn(p1, p2, m1, m2, false, false)
+			}
+		}
+	} else if def1 && !def2 {
+		// if player1 defends then use attack of player2 to
+		// determine the outcome
+		switch m1 {
+		case BLOCK:
+			// if p1 success, repair armor of p1, else p1
+			// takes damage of p2
+			if a1 > 0 {
+				p1.Armor += a1
+				printTurn(p1, p2, m1, m2, true, false)
+				fmt.Printf("%s repairs armor for %d points\n", p1.PlayerName, a1)
+			} else if a2 > 0 {
+				handleDamage(p1, a2)
+				printTurn(p1, p2, m1, m2, false, true)
+				fmt.Printf("%s deals %d damage\n", p2.PlayerName, a2)
+			} else {
+				printTurn(p1, p2, m1, m2, false, false)
+			}
+		case PARRY:
+			// if p1 success, p2 takes damage of p2 attack +
+			// damage of p1 counter, vise versa if fail
+			if a1 > 0 {
+				handleDamage(p2, a2+a1)
+				printTurn(p1, p2, m1, m2, true, false)
+				fmt.Printf("%s deals %d damage\n", p1.PlayerName, a2+a1)
+			} else if a2-a1 > 0 {
+				handleDamage(p1, a2-a1)
+				printTurn(p1, p2, m1, m2, false, true)
+				fmt.Printf("%s deals %d damage\n", p2.PlayerName, a2-a1)
+			} else {
+				printTurn(p1, p2, m1, m2, false, false)
+			}
+		case EVADE:
+			// if p1 success p1 heals a little, on fail p1
+			// takes damage of p2 attack
+			if a1 > 0 {
+				p1.Health += a1
+				printTurn(p1, p2, m1, m2, true, false)
+				fmt.Printf("%s heals %d damage\n", p1.PlayerName, a1)
+			} else if a2 > 0 {
+				handleDamage(p1, a2)
+				printTurn(p1, p2, m1, m2, false, true)
+				fmt.Printf("%s deals %d damage\n", p2.PlayerName, a2)
+			} else {
+				printTurn(p1, p2, m1, m2, false, false)
+			}
+		}
+	} else if def1 && def2 {
+		// nothing happens both players tried to defend
+		printTurn(p1, p2, m1, m2, false, false)
+		fmt.Printf("Nothing happens!\n")
+	}
+	printStats(p1)
+	printStats(p2)
+}
--- src/main.go
+++ src/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 
+	"github.iu.edu/evogelsa/go-ml-rpg/game"
 	"github.iu.edu/evogelsa/go-ml-rpg/ui"
 
 	"github.com/gorilla/mux"
@@ -23,8 +29,44 @@ func newRouter() *mux.Router {
 	return r
 }
 
+func getIntInput() int {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		choice, err := strconv.Atoi(input[:len(input)-1])
+		if err == nil && choice >= 0 && choice <= 5 {
+			return choice
+		} else {
+			fmt.Printf("Please input a valid integer selection\n")
+		}
+	}
+}
+
+func selectMove(p game.Class) game.Move {
+	fmt.Printf("%s select move:\n", p.PlayerName)
+	fmt.Printf("\t(0)Heavy attack\n\t(1)Quick attack\n\t(2)Standard attack\n")
+	fmt.Printf("\t(3)Block\n\t(4)Parry\n\t(5)Evade\n")
+	return game.Move(getIntInput())
+}
+
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	r := newRouter()
+
+	k := game.NewKnight("Ethan")
+	w := game.NewWizard("Renan")
+
+	game.PrintCharacter(k)
+	game.PrintCharacter(w)
+
+	for {
+		m1 := selectMove(k)
+		m2 := selectMove(w)
+		game.Turn(&k, &w, m1, m2)
+	}
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
